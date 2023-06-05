260dd9085e77f568699ea4cfb4cbae612da507d6
unknown <RuckingFS@outlook.es>
2023-06-05 07:07:32 -0300

    creando componentes para registrarse

--- front/src/components/Header/AccountSection/AccountSection.jsx
+++ front/src/components/Header/AccountSection/AccountSection.jsx
@@ -1,18 +1,14 @@
 import "./AccountSection.css";
-import bookmark_icon from "../../../assets/uiIcons/bookmark_icon.svg";
-import notification_icon from "../../../assets/uiIcons/notification_icon.svg";
-import upload_icon from "../../../assets/uiIcons/upload_icon.svg";
-import account_icon from "../../../assets/uiIcons/account_icon.svg";
+import NotRegistered from "./NotRegistered/NotRegistered";
+
+import Registered from "./Registered/Registered";
 
 const AccountSection = () => {
+  const userRegister = false;
+
   return (
     <div className="account">
-      <div className="account__container">
-        <img src={bookmark_icon} alt="" className="bookmark_icon" />
-        <img src={notification_icon} alt="" className="notification_icon" />
-        <img src={upload_icon} alt="" className="upload_icon" />
-        <img src={account_icon} alt="" className="account_icon" />
-      </div>
+      {!userRegister ? <NotRegistered /> : <Registered />}
     </div>
   );
 };
---
+++ front/src/components/Header/AccountSection/NotRegistered/NotRegistered.jsx
@@ -0,0 +1,12 @@
+import "./NotRegistered.css";
+
+const NotRegistered = () => {
+  return (
+    <div className="notRegistered__container">
+      <button className="login__container">Login</button>
+      <button className="signup__container">Sign Up</button>
+    </div>
+  );
+};
+
+export default NotRegistered;
